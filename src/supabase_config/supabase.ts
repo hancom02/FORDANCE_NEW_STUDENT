92854680d6488cbf5c4ddbd4efadafb89ddcda73
ngochan <20520484@gm.uit.edu.vn>
2024-12-20 00:47:17 +0700

    feat: config supabase

--- src/supabase_config/supabase.ts
+++ src/supabase_config/supabase.ts
@@ -1,5 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
+import { AppState } from 'react-native'
+import 'react-native-url-polyfill/auto'
 
 const supabaseUrl = 'https://dkasnzwgahkgoczktpfe.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrYXNuendnYWhrZ29jemt0cGZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzExNjEzMDMsImV4cCI6MjA0NjczNzMwM30.qzoF0VafeuIavRmpTsxzAzZvY7C-vZccexQ4b1MUIKI'
@@ -11,4 +13,12 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     persistSession: true,
     detectSessionInUrl: false,
   },
+})
+
+AppState.addEventListener('change', (state) => {
+  if (state === 'active') {
+    supabase.auth.startAutoRefresh()
+  } else {
+    supabase.auth.stopAutoRefresh()
+  }
 })
