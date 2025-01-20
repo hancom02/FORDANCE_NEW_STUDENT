0882a407fe34a6ea0c16fc524d950f1ec2d1dc9f
ngochan <20520484@gm.uit.edu.vn>
2025-01-20 21:50:42 +0700

    UI: add fullscreen feature, hadle when onPress video

--- src/screens/lession/views/video_player.tsx
+++ src/screens/lession/views/video_player.tsx
@@ -1,42 +1,91 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { View, StyleSheet, Modal, TouchableOpacity, Dimensions } from 'react-native';
-import Video from 'react-native-video';
+import Video, { OnPlaybackStateChangedData } from 'react-native-video';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Orientation from 'react-native-orientation-locker';
 
-const VideoPlayer = ({ uri, visible, setVisible }) => {
+const VideoPlayer = ({ uri, visible, setVisible, onProgress, onPlaying, timeStart = 0, onEnd }) => {
     const [fullscreen, setFullscreen] = useState(false);
+    const [paused, setPaused] = useState(true);
+    const videoRef = useRef(null);
 
     useEffect(() => {
-        // Thiết lập fullscreen thành true khi visible thay đổi và visible là true
         if (visible) {
-            setFullscreen(true);
+            // Mở fullscreen khi video hiển thị
+            handleFullscreen(true);
+        } else {
+            if (Orientation) {
+                Orientation.lockToPortrait();
+            }
         }
+    
+        return () => {
+            if (Orientation) {
+                Orientation.lockToPortrait(); // Đảm bảo trở về hướng dọc khi component unmount
+            }
+        };
     }, [visible]);
+
+    const handleFullscreen = (enable) => {
+        setFullscreen(enable);
+        if (enable) {
+            Orientation.lockToLandscape(); // Khóa màn hình ngang
+        } else {
+            Orientation.lockToPortrait(); // Khóa màn hình dọc
+        }
+    };
 
     const handleClose = () => {
         setVisible(false);
-        setFullscreen(false); // Reset fullscreen khi đóng video
+        handleFullscreen(false);
     };
 
-    const handleFullscreen = () => {
-        setFullscreen(!fullscreen);
+    const onLoad = (data) => {
+        if (videoRef.current) {
+            videoRef.current.seek(timeStart);
+        } else {
+            console.log('Video ref is not ready');
+        }
+    };
+    const onPlaybackStateChanged = (state: OnPlaybackStateChangedData) => {
+        if (state.isPlaying) {
+            console.log('VideoPlayer - onPlaybackStateChanged - playing');
+            onPlaying(); 
+        }
+        console.log('Playback state changed:', state); 
+    };
+
+    const togglePlayPause = () => {
+        setPaused((prevPaused) => !prevPaused); // Đảo trạng thái play/pause
     };
 
     return (
         <Modal animationType="slide" transparent={true} visible={visible}>
             <View style={styles.container}>
                 <Video
+                    ref={videoRef}
                     source={{ uri }}
                     style={fullscreen ? styles.fullscreenVideo : styles.video}
                     resizeMode="contain"
                     paused={false}
                     controls={true}
+                    onLoad={onLoad}
+                    onPlaybackStateChanged={onPlaybackStateChanged}
+                    onProgress={onProgress}
+                    onEnd={onEnd}
                 />
                 <View style={styles.closeButton}>
                     <TouchableOpacity onPress={handleClose}>
                         <Icon name="close" size={30} color="white" />
                     </TouchableOpacity>
                 </View>
+                {!fullscreen && (
+                    <View style={styles.fullscreenButton}>
+                        <TouchableOpacity onPress={() => handleFullscreen(true)}>
+                            <Icon name="arrows-alt" size={30} color="white" />
+                        </TouchableOpacity>
+                    </View>
+                )}
             </View>
         </Modal>
     );
@@ -53,11 +102,11 @@ const styles = StyleSheet.create({
     },
     video: {
         width: width,
-        height: height,
+        height: height / 2,
     },
     fullscreenVideo: {
-        width: width,
-        height: height / 2, // Chiều cao khi ở chế độ toàn màn hình
+        width: height, 
+        height: width,
     },
     closeButton: {
         position: 'absolute',
